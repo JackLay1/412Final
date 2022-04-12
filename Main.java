8775a616baa2cff62aa7381aae691ed8688aae1f
JackLay1 <42285595+JackLay1@users.noreply.github.com>
2022-04-12 09:55:48 -0600

    Addded Main and Created View Class

---
+++ Main.java
@@ -0,0 +1,233 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    char game[][] = new char[3][3];
+    int count = 0;
+    JButton blist[] = new JButton[9];
+    boolean win = false;
+    public void check(){
+        if(count > 4){
+            if(game[0][0] == game[0][1] && game[0][1] == game[0][2]){
+                System.out.println("WIN DECLARED (1)");
+                halt();
+                win = true;
+            }
+            else if(game[1][0] == game[1][1] && game[1][1] == game[1][2]){
+                System.out.println("WIN DECLARED (2)");
+                halt();
+                win = true;
+            }
+            else if(game[2][0] == game[2][1] && game[2][1] == game[2][2]){
+                System.out.println("WIN DECLARED (3)");
+                halt();
+                win = true;
+            }
+            else if(game[0][0] == game[1][0] && game[1][0] == game[2][0]){
+                System.out.println("WIN DECLARED (4)");
+                halt();
+                win = true;
+            }
+            else if(game[0][1] == game[1][1] && game[1][1] == game[2][1]){
+                System.out.println("WIN DECLARED (5)");
+                halt();
+                win = true;
+            }
+            else if(game[0][2] == game[1][2] && game[1][2] == game[2][2]){
+                System.out.println("WIN DECLARED (6)");
+                halt();
+                win = true;
+            }
+            else if(game[0][0] == game[1][1] && game[1][1] == game[2][2]){
+                System.out.println("WIN DECLARED (7)");
+                halt();
+                win = true;
+            }
+            else if(game[0][2] == game[1][1] && game[1][1] == game[2][0]){
+                System.out.println("WIN DECLARED (8)");
+                halt();
+                win = true;
+            }
+        }
+        if(count == 9 && !win){
+            System.out.println("TIE DECLARED");
+        }
+    }
+    public void clicked(JButton b){
+        check();
+        if (count % 2 == 0) {
+            b.setText("x");
+            b.setFont(new Font("Arial", Font.BOLD, 70));
+            b.setEnabled(false);
+            count++;
+        }
+        else {
+            b.setText("O");
+            b.setFont(new Font("SANS SERIF", Font.BOLD, 69));
+            b.setEnabled(false);
+            count++;
+        }
+        check();
+    }
+    public void halt(){
+        System.out.println("Halting");
+        for(int i=0; i<9; i++){
+            blist[i].setEnabled(false);
+        }
+    }
+    public void go(){
+        JFrame frame = new JFrame();
+        frame.setSize(600,600);
+        JPanel container = new JPanel();
+        container.setLayout(new GridLayout(3,3));
+        JButton b1 = new JButton();
+        blist[0] = b1;
+        JButton b2 = new JButton();
+        blist[1] = b2;
+        JButton b3 = new JButton();
+        blist[2] = b3;
+        JButton b4 = new JButton();
+        blist[3] = b4;
+        JButton b5 = new JButton();
+        blist[4] = b5;
+        JButton b6 = new JButton();
+        blist[5] = b6;
+        JButton b7 = new JButton();
+        blist[6] = b7;
+        JButton b8 = new JButton();
+        blist[7] = b8;
+        JButton b9 = new JButton();
+        blist[8] = b9;
+        container.add(b1);
+        container.add(b2);
+        container.add(b3);
+        container.add(b4);
+        container.add(b5);
+        container.add(b6);
+        container.add(b7);
+        container.add(b8);
+        container.add(b9);
+        frame.add(container, BorderLayout.CENTER);
+        frame.setVisible(true);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b1);
+                container.setEnabled(false);
+                if (count % 2 == 0) {
+                    game[0][0] = 'x';
+                }
+                else {
+                    game[0][0] = 'O';
+                }
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b2);
+                if (count % 2 == 0) {
+                    game[0][1] = 'x';
+                }
+                else {
+                    game[0][1] = 'O';
+                }
+            }
+        });
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b3);
+                if (count % 2 == 0) {
+                    game[0][2] = 'x';
+                }
+                else {
+                    game[0][2] = 'O';
+                }
+            }
+        });
+        b4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b4);
+                if (count % 2 == 0) {
+                    game[1][0] = 'x';
+                }
+                else {
+                    game[1][0] = 'O';
+                }
+            }
+        });
+        b5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b5);
+                if (count % 2 == 0) {
+                    game[1][1] = 'x';
+                }
+                else {
+                    game[1][1] = 'O';
+                }
+            }
+        });
+        b6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b6);
+                if (count % 2 == 0) {
+                    game[1][2] = 'x';
+                }
+                else {
+                    game[1][2] = 'O';
+                }
+            }
+        });
+        b7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b7);
+                if (count % 2 == 0) {
+                    game[2][0] = 'x';
+                }
+                else {
+                    game[2][0] = 'O';
+                }
+            }
+        });
+        b8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b8);
+                if (count % 2 == 0) {
+                    game[2][1] = 'x';
+                }
+                else {
+                    game[2][1] = 'O';
+                }
+            }
+        });
+        b9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b9);
+                if (count % 2 == 0) {
+                    game[2][2] = 'x';
+                }
+                else {
+                    game[2][2] = 'O';
+                }
+            }
+        });
+
+
+    }
+
+    public static void main(String[] args) {
+        new Main().go();
+    }
+}
