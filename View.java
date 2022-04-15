aecc9347b308e66fab5eeabeba9bac9f9cefed94
JackLay1 <42285595+JackLay1@users.noreply.github.com>
2022-04-14 20:10:18 -0600

    another gui update
    edited main menu frame

--- View.java
+++ View.java
@@ -4,20 +4,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View {
+    JButton[][] buttons = new JButton[3][3];
     char game[][] = new char[3][3];
     int count = 0;
     JButton blist[] = new JButton[9];
-    public static void mmenu(){
+    public void mmenu(){
         JFrame frame = new JFrame();
         frame.setSize(600,600);
         JPanel container = new JPanel();
         JTextField ip = new JTextField();
         JButton send = new JButton();
         send.setText("SEND REQUEST");
-        container.add(ip);
-        container.add(send);
-        frame.add(container, BorderLayout.CENTER);
+        frame.add(ip, BorderLayout.SOUTH);
+        frame.add(send, BorderLayout.CENTER);
         frame.setVisible(true);
+
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pip = ip.getText();
+                System.out.println(pip);
+            }
+        });
     }
     public void go(){
         JFrame frame = new JFrame();
@@ -26,22 +34,31 @@ public class View {
         container.setLayout(new GridLayout(3,3));
         JButton b1 = new JButton();
         blist[0] = b1;
+        buttons[0][0] = b1;
         JButton b2 = new JButton();
         blist[1] = b2;
+        buttons[0][1] = b2;
         JButton b3 = new JButton();
         blist[2] = b3;
+        buttons[0][2] = b3;
         JButton b4 = new JButton();
         blist[3] = b4;
+        buttons[1][0] = b4;
         JButton b5 = new JButton();
         blist[4] = b5;
+        buttons[1][1] = b5;
         JButton b6 = new JButton();
         blist[5] = b6;
+        buttons[1][2] = b6;
         JButton b7 = new JButton();
         blist[6] = b7;
+        buttons[2][0] = b7;
         JButton b8 = new JButton();
         blist[7] = b8;
+        buttons[2][1] = b8;
         JButton b9 = new JButton();
         blist[8] = b9;
+        buttons[2][2] = b9;
         container.add(b1);
         container.add(b2);
         container.add(b3);
@@ -171,6 +188,6 @@ public class View {
     }
 
     public static void main(String[] args) {
-        mmenu();
+        new View().mmenu();
     }
 }
