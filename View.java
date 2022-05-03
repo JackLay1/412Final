2ea9e2ec9d2edaf95f1dd16d6e67cdbad11a4937
JackLay1 <42285595+JackLay1@users.noreply.github.com>
2022-05-02 19:59:07 -0600

    getIP()
    getIP should return what is in the textbox

--- View.java
+++ View.java
@@ -18,6 +18,231 @@ public class View {
     int count = 1;
     JButton blist[] = new JButton[9];
 
+    public void start(){
+        JFrame frame = new JFrame("Tx3");
+        frame.setSize(600,600);
+        container21.add(prompt);
+        container21.add(ip);
+        frame.add(send, BorderLayout.EAST);
+        frame.add(container21, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+    };
+    public String getIP(){
+        return ip.getText();
+    };
+    public void goToGame(){
+        JFrame frame = new JFrame();
+        frame.getContentPane().removeAll();
+        JPanel container = new JPanel();
+        container.setLayout(new GridLayout(3,3));
+        JPanel buttonss = new JPanel();
+        JButton b1 = new JButton();
+        blist[0] = b1;
+        buttons[0][0] = b1;
+        JButton b2 = new JButton();
+        blist[1] = b2;
+        buttons[0][1] = b2;
+        JButton b3 = new JButton();
+        blist[2] = b3;
+        buttons[0][2] = b3;
+        JButton b4 = new JButton();
+        blist[3] = b4;
+        buttons[1][0] = b4;
+        JButton b5 = new JButton();
+        blist[4] = b5;
+        buttons[1][1] = b5;
+        JButton b6 = new JButton();
+        blist[5] = b6;
+        buttons[1][2] = b6;
+        JButton b7 = new JButton();
+        blist[6] = b7;
+        buttons[2][0] = b7;
+        JButton b8 = new JButton();
+        blist[7] = b8;
+        buttons[2][1] = b8;
+        JButton b9 = new JButton();
+        blist[8] = b9;
+        buttons[2][2] = b9;
+        container.add(b1);
+        container.add(b2);
+        container.add(b3);
+        container.add(b4);
+        container.add(b5);
+        container.add(b6);
+        container.add(b7);
+        container.add(b8);
+        container.add(b9);
+        frame.add(container, BorderLayout.CENTER);
+        buttonss.add(again);
+        buttonss.add(quit);
+        frame.add(buttonss, BorderLayout.SOUTH);
+        buttonss.setVisible(true);
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b1);
+                container.setEnabled(false);
+                if (count % 2 == 0) {
+                    game[0][0] = 'x';
+                }
+                else {
+                    game[0][0] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b2);
+                if (count % 2 == 0) {
+                    game[0][1] = 'x';
+                }
+                else {
+                    game[0][1] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b3);
+                if (count % 2 == 0) {
+                    game[0][2] = 'x';
+                }
+                else {
+                    game[0][2] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        b4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b4);
+                if (count % 2 == 0) {
+                    game[1][0] = 'x';
+                }
+                else {
+                    game[1][0] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        b5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b5);
+                if (count % 2 == 0) {
+                    game[1][1] = 'x';
+                }
+                else {
+                    game[1][1] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        b6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b6);
+                if (count % 2 == 0) {
+                    game[1][2] = 'x';
+                }
+                else {
+                    game[1][2] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        b7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b7);
+                if (count % 2 == 0) {
+                    game[2][0] = 'x';
+                }
+                else {
+                    game[2][0] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        b8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b8);
+                if (count % 2 == 0) {
+                    game[2][1] = 'x';
+                }
+                else {
+                    game[2][1] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        b9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicked(b9);
+                if (count % 2 == 0) {
+                    game[2][2] = 'x';
+                }
+                else {
+                    game[2][2] = 'O';
+                }
+                WaitForOtherPlayer();
+                DISPLAYBOARD();
+            }
+        });
+        frame.repaint();
+        frame.validate();
+    }
+});
+        again.addActionListener(new ActionListener() {
+@Override
+public void actionPerformed(ActionEvent e) {
+        for(int i=0; i<9; i++){
+        blist[i].setText("");
+        blist[i].setEnabled(true);
+        count=1;
+        }
+        for (int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+        game[i][j]='\u0000';
+        }
+        }
+        }
+        });
+        quit.addActionListener(new ActionListener() {
+@Override
+public void actionPerformed(ActionEvent e) {
+        frame.getContentPane().removeAll();
+        container21.add(prompt);
+        container21.add(ip);
+        frame.add(send, BorderLayout.EAST);
+        frame.add(container21, BorderLayout.CENTER);
+        frame.repaint();
+        frame.validate();
+        }
+        });
+    };
+
     public void halt(){
         for(int i=0; i<9; i++){
             blist[i].setEnabled(false);
@@ -38,227 +263,11 @@ public class View {
         }
     }
     public void finalgo(){
-        JFrame frame = new JFrame("Tx3");
-        frame.setSize(600,600);
-        container21.add(prompt);
-        container21.add(ip);
-        frame.add(send, BorderLayout.EAST);
-        frame.add(container21, BorderLayout.CENTER);
 
-        frame.setVisible(true);
 
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        send.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String pip = ip.getText();
-                System.out.println(pip);
-                frame.getContentPane().removeAll();
-                JPanel container = new JPanel();
-                container.setLayout(new GridLayout(3,3));
-                JPanel buttonss = new JPanel();
-                JButton b1 = new JButton();
-                blist[0] = b1;
-                buttons[0][0] = b1;
-                JButton b2 = new JButton();
-                blist[1] = b2;
-                buttons[0][1] = b2;
-                JButton b3 = new JButton();
-                blist[2] = b3;
-                buttons[0][2] = b3;
-                JButton b4 = new JButton();
-                blist[3] = b4;
-                buttons[1][0] = b4;
-                JButton b5 = new JButton();
-                blist[4] = b5;
-                buttons[1][1] = b5;
-                JButton b6 = new JButton();
-                blist[5] = b6;
-                buttons[1][2] = b6;
-                JButton b7 = new JButton();
-                blist[6] = b7;
-                buttons[2][0] = b7;
-                JButton b8 = new JButton();
-                blist[7] = b8;
-                buttons[2][1] = b8;
-                JButton b9 = new JButton();
-                blist[8] = b9;
-                buttons[2][2] = b9;
-                container.add(b1);
-                container.add(b2);
-                container.add(b3);
-                container.add(b4);
-                container.add(b5);
-                container.add(b6);
-                container.add(b7);
-                container.add(b8);
-                container.add(b9);
-                frame.add(container, BorderLayout.CENTER);
-                buttonss.add(again);
-                buttonss.add(quit);
-                buttonss.add(URTURN);
-                buttonss.add(NOTU);
-                frame.add(buttonss, BorderLayout.SOUTH);
-                buttonss.setVisible(true);
 
-                b1.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b1);
-                        container.setEnabled(false);
-                        if (count % 2 == 0) {
-                            game[0][0] = 'x';
-                        }
-                        else {
-                            game[0][0] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                b2.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b2);
-                        if (count % 2 == 0) {
-                            game[0][1] = 'x';
-                        }
-                        else {
-                            game[0][1] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                b3.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b3);
-                        if (count % 2 == 0) {
-                            game[0][2] = 'x';
-                        }
-                        else {
-                            game[0][2] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                b4.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b4);
-                        if (count % 2 == 0) {
-                            game[1][0] = 'x';
-                        }
-                        else {
-                            game[1][0] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                b5.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b5);
-                        if (count % 2 == 0) {
-                            game[1][1] = 'x';
-                        }
-                        else {
-                            game[1][1] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                b6.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b6);
-                        if (count % 2 == 0) {
-                            game[1][2] = 'x';
-                        }
-                        else {
-                            game[1][2] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                b7.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b7);
-                        if (count % 2 == 0) {
-                            game[2][0] = 'x';
-                        }
-                        else {
-                            game[2][0] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                b8.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b8);
-                        if (count % 2 == 0) {
-                            game[2][1] = 'x';
-                        }
-                        else {
-                            game[2][1] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                b9.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        clicked(b9);
-                        if (count % 2 == 0) {
-                            game[2][2] = 'x';
-                        }
-                        else {
-                            game[2][2] = 'O';
-                        }
-                        WaitForOtherPlayer();
-                        DISPLAYBOARD();
-                    }
-                });
-                frame.repaint();
-                frame.validate();
-            }
-        });
-        again.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                for(int i=0; i<9; i++){
-                    blist[i].setText("");
-                    blist[i].setEnabled(true);
-                    count=1;
-                }
-                for (int i=0; i<3; i++){
-                    for(int j=0; j<3; j++){
-                        game[i][j]='\u0000';
-                    }
-                }
-            }
-        });
-        quit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.getContentPane().removeAll();
-                container21.add(prompt);
-                container21.add(ip);
-                frame.add(send, BorderLayout.EAST);
-                frame.add(container21, BorderLayout.CENTER);
-                frame.repaint();
-                frame.validate();
-            }
-        });
+
+
         URTURN.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
